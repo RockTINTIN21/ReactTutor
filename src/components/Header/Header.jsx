4bee1e3ce60fd362b4efaf8084f1a6a77f960883
sasha <sashalexjr@gmail.com>
2024-10-27 23:17:54 +0500

    Исправлена проблема с адаптивной высотой у блока main

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,26 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 import './Header.css';
 import Logo from '../../assets/icons/Logo.png';
 import gitLogo from '../../assets/icons/gitLogo.png';
+import {useEffect, useRef, useState} from "react";
 function Header(){
+    const navbar = useRef(null);
+    // const [navbar,setNavbar] = useState('');
+    // useEffect(()=>{
+    //
+    //     // setNavbar(document.querySelector('.navbar').style.height);
+    // },[])
+    // useEffect(() => {
+    //     if(navbar){
+    //         const height = navbar.current.getBoundingClientRect().height;
+    //         console.log('navbar',height);
+    //         // console.log('navbar:',getComputedStyle(navbar.current).getPropertyValue('height'));
+    //     }
+    //
+    // },[navbar])
     return(
-        <>
-            <Navbar collapseOnSelect expand="lg" className="">
-                    <Navbar.Brand href="/">
+
+            <Navbar collapseOnSelect className='p-0 m-0'>
+                    <Navbar.Brand href="/" >
                         <img
                             alt="ReactTutor"
                             src={Logo}
@@ -15,19 +30,18 @@ function Header(){
                         React.TutorAI
                     </Navbar.Brand>
                     <Navbar.Toggle className='custom-toggler' aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Navbar.Collapse id="responsive-navbar-nav" className='p-0 test' >
                         <Nav className="me-auto"></Nav>
-                        <Nav className='text-center'>
-                            <Nav.Link href="#deets" className='ms-md-5'>Викторина</Nav.Link>
-                            <Nav.Link href="#deets" className='ms-md-5'>Инструменты</Nav.Link>
-                            <Nav.Link href="#deets" className='ms-md-5'>О проекте</Nav.Link>
-                            <Nav.Link className="ms-md-5">
+                        <Nav className='text-center '>
+                            <Nav.Link href="#deets" className='ms-md-5  p-0'>Викторина</Nav.Link>
+                            <Nav.Link href="#deets" className='ms-md-5  p-0'>Инструменты</Nav.Link>
+                            <Nav.Link href="#deets" className='ms-md-5 p-0'>О проекте</Nav.Link>
+                            <Nav.Link className="ms-md-5 p-0">
                                 <img src={gitLogo} alt="gitLogo"/>
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
             </Navbar>
-        </>
     )
 }
 export default Header;
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -5,78 +5,101 @@ import './Chat.css';
 import styles from './Chat.module.css';
 import history from '../../assets/icons/history.png';
 import send from '../../assets/icons/send.png';
-import {Button, Card, CardGroup, Carousel, Col, Form, Row} from "react-bootstrap";
+import send_lock from '../../assets/icons/send_lock.png';
+import {Button, Form, Row} from "react-bootstrap";
 import {useEffect, useRef, useState} from "react";
 import {Helmet} from "react-helmet";
 import userAvatar from '../../assets/icons/avatar.png';
 import ChangeButtonColor from "../../components/ChangeButtonColor/ChangeButtonColor.jsx";
 
 function Chat() {
-    const textareaRef = useRef(null);
-    const [chatInputElement, setChatInputElement] = useState(null);
-    const [formControlChat, setFormControlChat] = useState(null);
+    const chatInputElementRef = useRef(null);
+    const submitButton = useRef(null);
+    const mainContentRef = useRef(null);
+    const [formControlChat,setFormControlChat] = useState(null);
     const [wrapperButtonSubmit, setWrapperButtonSubmit] = useState(null);
-    const [size, setSize] = useState([window.innerWidth]);
-    const testRef = useRef(null);
+    const [navbarHeight,setNavbarHeight] = useState(null);
+    const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+    const [value, setValue] = useState(0);
+    const [test,setTest] = useState(0)
     useEffect(() => {
-        setTimeout(() => {
-            setChatInputElement(document.querySelector('.chat_input'));
-            setFormControlChat(document.querySelector('.form-control__chat'));
-            setWrapperButtonSubmit(document.querySelector('.wrapper-buttonSubmit'));
-        },0);
-
-        // changeInputWidth();
-        const resizeHandler = () => setSize([window.innerWidth])
+        setNavbarHeight(document.querySelector('.navbar'));
+        setFormControlChat(document.querySelector('.form-control__chat'));
+        setWrapperButtonSubmit(document.querySelector('.wrapper-buttonSubmit'));
+        const resizeHandler = () => setSize([window.innerWidth, window.innerHeight])
         window.addEventListener('resize', resizeHandler);
         return ()=>{
-            window.removeEventListener('resize', () => setSize([window.innerWidth]));
+            window.removeEventListener('resize', () => setSize([window.innerWidth ,window.innerHeight]));
         }
     }, []);
     useEffect(() => {
         changeInputWidth();
-        console.log('test')
-    },[size])
+    },[size,formControlChat]);
+
+    const handleChangeTextarea = (e) =>{
+        if(e.target.value.length){
+            submitButton.current.classList.remove('disabled');
+        }else{
+            submitButton.current.classList.add('disabled');
+        }
+    }
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (navbarHeight) {
+                console.log('test',navbarHeight.scrollHeight);
+                const mainContentHeight = size[1] - parseFloat(getComputedStyle(navbarHeight).height);
+                setTest(mainContentHeight);
+                mainContentRef.current.style.height = `${mainContentHeight}px`;
+            }
+        }, 100); // Задержка в 100 мс
+        return () => clearTimeout(timeoutId);
+    }, [navbarHeight, size]);
     const changeInputWidth = () => {
-        if (formControlChat && chatInputElement) {
-            console.log('вызвано!');
-            formControlChat.style.width = `${chatInputElement.offsetWidth}px`;
+        if(formControlChat){
+            formControlChat.style.width = `${chatInputElementRef.current.offsetWidth}px`;
         }
     };
-
-    // Обработка ввода в textarea
-    const handleInput = () => {
-        const textarea = textareaRef.current;
-        textarea.style.height = 'auto';
-        textarea.style.height = `${textarea.scrollHeight}px`;
-
+    const handleInput = (e) => {
+        setValue(e.target.value);
         const computedStyles = window.getComputedStyle(formControlChat);
         const minHeight = parseFloat(computedStyles.minHeight);
         const height = parseFloat(computedStyles.height);
-
+        formControlChat.style.height = `${formControlChat.scrollHeight}px`;
         if (height > minHeight) {
             wrapperButtonSubmit.style.alignItems = 'end';
+            if(height >= 192) {
+                formControlChat.style.overflowY = `scroll`;
+            }
         } else {
+            formControlChat.style.overflowY = `hidden`;
             wrapperButtonSubmit.style.alignItems = 'center';
         }
     };
-
+    useEffect(() => {
+        if(value.length === 0){
+            formControlChat.style.overflowY = `hidden`;
+            wrapperButtonSubmit.style.alignItems = 'center';
+            formControlChat.style.height = '3rem';
+        }
+    },[value,formControlChat,wrapperButtonSubmit])
     return (
-        <div className='h-100 p-md-0 p-3'>
+        <div className='h-100 p-3 p-md-0'>
             <Helmet><title>Чат</title></Helmet>
             <Header />
-            <div className="mt-2 row">
-                <aside className='sidebar p-2 col-12 col-md-3'>
+            <Row>
+                <aside className='sidebar p-2 col-12 col-md-3 h-25'>
                     <div className="d-flex justify-content-between align-items-center">
                         <h5>История тем общения</h5>
                         <ChangeButtonColor path={history} alt={'История'} />
                     </div>
                     <ul className={styles.history__theme}>
-                        <li>Знакомство</li>
+                        <li><a href="">Знакомство</a></li>
                         <li>Выбор направления</li>
                         <li>Что такое html</li>
+                        <li>{test}</li>
                     </ul>
                 </aside>
-                <main className="col-12 col-md-9 p-2">
+                <main className="col-12 col-md-9 p-2" ref={mainContentRef}>
                     <div className="chat_header text-md-start text-center">
                         <h3>Чат с ReactTutor</h3>
                         <div className="date pt-1"><span>20.10.2024</span></div>
@@ -110,18 +133,21 @@ function Chat() {
                         </div>
                     </div>
 
-                    <Form className='mt-5 container-fluid '>
-                        <Form.Group className="mb-3 row chat_input" controlId="formChat">
-                            <div className="col-10 col-md-11 ps-0" ref={testRef}>
-                                <Form.Control as='textarea' className='form-control__chat' rows={1} onInput={handleInput} ref={textareaRef} placeholder="Ваш вопрос" />
+                    <Form className='mt-5 container-fluid'>
+                        <Form.Group className="mb-3 row chat_input" ref={chatInputElementRef} controlId="formChat">
+                            <div className="col-10 col-md-11 ps-0" >
+                                <Form.Control as='textarea' className='form-control__chat' rows={1} onInput={handleInput} onChange={handleChangeTextarea} placeholder="Ваш вопрос" />
                             </div>
-                            <div className="col-2 ps-md-4 ps-0 m-0 col-md-1 d-flex wrapper-buttonSubmit">
-                                <ChangeButtonColor isSubmit={true} path={send} alt={'Отправить'} />
+                            <div className="col-2 ps-md-4 ps-lg-2 ps-xxl-3 ps-0 m-0 col-md-1 d-flex wrapper-buttonSubmit">
+                                {/*<ChangeButtonColor isSubmit={true} path={buttonSend} alt={'Отправить'} />*/}
+                                <Button className='button_submit disabled' ref={submitButton}>
+                                    <img src={send} alt='Отправить'/>
+                                </Button>
                             </div>
                         </Form.Group>
                     </Form>
                 </main>
-            </div>
+            </Row>
         </div>
     );
 }
