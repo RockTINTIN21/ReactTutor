4af138070b3efbe4f8dd0ce90642cec2cf060721
sasha <sashalexjr@gmail.com>
2024-10-31 18:57:53 +0500

    Исправлена проблема с высотой блока чата на мобильных устройствах.

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,16 +1,13 @@
 import Header from "../../components/Header/Header.jsx";
-import VantaBackground from "../../components/VantaBackground/VantaBackground.jsx";
 import largeLogo from '../../assets/icons/largeLogo.png';
 import './Chat.css';
 import styles from './Chat.module.css';
 import history from '../../assets/icons/history.png';
 import send from '../../assets/icons/send.png';
-import send_lock from '../../assets/icons/send_lock.png';
 import {Button, Form, Row} from "react-bootstrap";
 import {useEffect, useLayoutEffect, useRef, useState} from "react";
 import {Helmet} from "react-helmet";
 import userAvatar from '../../assets/icons/avatar.png';
-import ChangeButtonColor from "../../components/ChangeButtonColor/ChangeButtonColor.jsx";
 import useElementHeight from "../../hooks/useElementHeight.js";
 import useElementWidth from "../../hooks/useElementWidth.js";
 
@@ -22,16 +19,15 @@ function Chat() {
     const [formValue, setFormValue] = useState(0);
     const [clientChatTotalHeight,setClientChatTotalHeight] = useState(null)
     const [navbarRef,navbarHeight] = useElementHeight(null);
-    const [chatMaxHeightRef,clientChatMaxHeight] = useElementHeight(null)
+    const [chatRef,clientChatMaxHeight] = useElementHeight(null)
     const [formControlChatRef, formControlChatWidth] = useElementWidth(null)
     const wrapperButtonSubmitRef = useRef(null);
     const chatFormWrapperRef = useRef(null);
-    // const [chatFormWrapperRef, chatFormWrapperWidth] = useElementWidth(null);
-    // const [mainContentRef, mainContentWidth] = useElementWidth(null);
+    const sideBarRef = useRef(null)
     useEffect(() => {
         const resizeHandler = () => setClientWindowSize([window.innerWidth, window.innerHeight])
         window.addEventListener('resize', resizeHandler);
-        const messages = chatMaxHeightRef.current.querySelectorAll('.message');
+        const messages = chatRef.current.querySelectorAll('.message');
         const resizeObserver = new ResizeObserver((entries) => {
             let totalHeight = 0;
             for (let entry of entries) {
@@ -49,17 +45,24 @@ function Chat() {
         }
     },[])
 
+    // Хук вызова фунции changeInputWidth при изменении размера экрана пользователя.
     useEffect(() => {
         changeInputWidth();
     },[clientWindowSize]);
 
+    // Хук автоматического скролла в блоке чата.
     useLayoutEffect(() => {
-        console.log('CurrentHeight:',clientChatTotalHeight,'maxHeight:',clientChatMaxHeight)
         if(clientChatTotalHeight > clientChatMaxHeight){
-
+            chatRef.current.style.overflowY = `scroll`;
+            chatRef.current.classList.add('pe-3');
+        }else{
+            chatRef.current.classList.remove('pe-3');
+            chatRef.current.style.overflowY = `hidden`;
         }
 
-    }, [clientChatMaxHeight,clientChatTotalHeight]);
+    }, [clientChatMaxHeight,clientChatTotalHeight,formValue]);
+
+    // Функция блокировки кнопки отправки.
     const handleChangeTextarea = (e) =>{
         if(e.target.value.length){
             submitButton.current.classList.remove('disabled');
@@ -67,21 +70,31 @@ function Chat() {
             submitButton.current.classList.add('disabled');
         }
     }
+    // Хук изменения высоты блока чата с сообщениями.
     useLayoutEffect(() => {
-        mainContentRef.current.style.height = `${((clientWindowSize[1] - navbarHeight) * 0.99) - chatFormWrapperRef.current.getBoundingClientRect().height}px`;
-    },[navbarHeight])
+        if(clientWindowSize[1] <= 767){
+            console.log('Вызвано')
+            mainContentRef.current.style.height = `${clientWindowSize[1] + 100}px`;
+        }else{
+            mainContentRef.current.style.height = `${((clientWindowSize[1] - navbarHeight) * 0.99) - chatFormWrapperRef.current.getBoundingClientRect().height}px`;
+        }
 
+    },[navbarHeight,clientWindowSize])
+
+    // Хук изменения длины окна чата с сообщениями.
     useEffect(() => {
         const mainContentWidth = mainContentRef.current.getBoundingClientRect().width;
         chatFormWrapperRef.current.style.width = `${mainContentWidth}px`;
     }, [clientWindowSize]);
 
 
+    // Функция измения длины поля ввода.
     const changeInputWidth = () => {
         if(formControlChatRef){
             formControlChatRef.current.style.width = `${chatInputElementRef.current.offsetWidth}px`;
         }
     };
+    // Функция измения высоты поля ввода и включения скролла в поле ввода.
     const handleInput = (e) => {
         setFormValue(e.target.value);
         const computedStyles = window.getComputedStyle(formControlChatRef.current);
@@ -90,14 +103,16 @@ function Chat() {
         formControlChatRef.current.style.height = `${formControlChatRef.current.scrollHeight}px`;
         if (height > minHeight) {
             wrapperButtonSubmitRef.current.style.alignItems = 'end';
-            if(height >= 192) {
+            if(height > 192) {
                 formControlChatRef.current.style.overflowY = `scroll`;
+                formControlChatRef.current.style.height = `250px`;
             }
         } else {
             formControlChatRef.current.style.overflowY = `hidden`;
             wrapperButtonSubmitRef.current.style.alignItems = 'center';
         }
     };
+    // Хук сброса высоты по умолчанию.
     useLayoutEffect(() => {
         if(!formValue.length){
             formControlChatRef.current.style.overflowY = `hidden`;
@@ -106,15 +121,39 @@ function Chat() {
         }
     },[formValue,formControlChatRef,wrapperButtonSubmitRef])
 
+    //Функция скрытия сайдбара
+    const handleShowSidebar = () =>{
+        // const test = () =>{
+        //     if(sideBarRef.current.style.width !== 'auto'){
+        //         sideBarRef.current.style.width = '0px';
+        //         // sideBarRef.current.style.display = 'none';
+        //     }else{
+        //         sideBarRef.current.style.width = 'auto';
+        //         // sideBarRef.current.style.display = 'block';
+        //     }
+        // }
+        // requestAnimationFrame(test)
+
+        if(sideBarRef.current.style.width !== 'auto'){
+            sideBarRef.current.style.width = '0px';
+            sideBarRef.current.style.display = 'none';
+        }else{
+            sideBarRef.current.style.width = 'auto';
+            sideBarRef.current.style.display = 'block';
+        }
+    }
+
     return (
         <div className='h-100 p-3 p-md-0'>
             <Helmet><title>Чат</title></Helmet>
             <Header navbarHeight={navbarHeight} refNavbar={navbarRef}/>
             <Row className=''>
-                <aside className='sidebar p-2 col-12 col-md-3 h-25'>
+                <aside className='sidebar col-12 col-md-3' ref={sideBarRef}>
                     <div className="d-flex justify-content-between align-items-center">
                         <h5>История тем общения</h5>
-                        <ChangeButtonColor path={history} alt={'История'} />
+                        <Button className='clear__button'>
+                            <img src={history} alt='История' onClick={handleShowSidebar} />
+                        </Button>
                     </div>
                     <ul className={styles.history__theme}>
                         <li><a href="">Знакомство</a></li>
@@ -124,13 +163,13 @@ function Chat() {
                         <li>clientHeight: {clientWindowSize[1]}</li>
                     </ul>
                 </aside>
-                <main className="col-12 col-md-9 p-2 d-flex flex-column wrapperChat" ref={mainContentRef}>
+                <main className="col-12 col-md-9 ps-2 pe-0 d-flex flex-column wrapperChat" ref={mainContentRef}>
                     <div className="chat_header text-md-start text-center">
                         <h3>Чат с ReactTutor</h3>
                         <div className="date pt-1"><span>20.10.2024</span></div>
                         <hr />
                     </div>
-                    <div className="chat h-100 mh-100" ref={chatMaxHeightRef}>
+                    <div className="chat h-100 mh-100 pb-5 mb-2" ref={chatRef}>
                         <div className="message message__reactTutor d-flex flex-column w-100 align-items-start">
                             <div className='header_message header_message__reactTutor'>
                                 <img src={largeLogo} width='35px' alt="largeLogo"/>
@@ -192,17 +231,163 @@ function Chat() {
                                 <span>20:27</span>
                             </div>
                         </div>
-
+                        <div className="message message__user d-flex flex-column w-100 align-items-end">
+                            <div className='header_message header_message__user'>
+                                <span className='pe-2'>Александр</span>
+                                <img src={userAvatar} width='35px' alt="largeLogo"/>
+                            </div>
+                            <div className='content_message content_message__user mt-3'>
+                                <p className='m-0'>Пожалуй я выберу Js</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:29</span>
+                            </div>
+                        </div>
+                        <div className="message message__reactTutor d-flex flex-column w-100 align-items-start">
+                            <div className='header_message header_message__reactTutor'>
+                                <img src={largeLogo} width='35px' alt="largeLogo"/>
+                                <span className='ps-2'>ReactTutor</span>
+                            </div>
+                            <div className='content_message content_message__reactTutor mt-3'>
+                                <p className='m-0'>Привет! Я ReactTutor, твой помощник в изучении фронтенда.</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:27</span>
+                            </div>
+                        </div>
+                        <div className="message message__user d-flex flex-column w-100 align-items-end">
+                            <div className='header_message header_message__user'>
+                                <span className='pe-2'>Александр</span>
+                                <img src={userAvatar} width='35px' alt="largeLogo"/>
+                            </div>
+                            <div className='content_message content_message__user mt-3'>
+                                <p className='m-0'>Пожалуй я выберу Js</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:29</span>
+                            </div>
+                        </div>
+                        <div className="message message__reactTutor d-flex flex-column w-100 align-items-start">
+                            <div className='header_message header_message__reactTutor'>
+                                <img src={largeLogo} width='35px' alt="largeLogo"/>
+                                <span className='ps-2'>ReactTutor</span>
+                            </div>
+                            <div className='content_message content_message__reactTutor mt-3'>
+                                <p className='m-0'>Привет! Я ReactTutor, твой помощник в изучении фронтенда.</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:27</span>
+                            </div>
+                        </div>
+                        <div className="message message__user d-flex flex-column w-100 align-items-end">
+                            <div className='header_message header_message__user'>
+                                <span className='pe-2'>Александр</span>
+                                <img src={userAvatar} width='35px' alt="largeLogo"/>
+                            </div>
+                            <div className='content_message content_message__user mt-3'>
+                                <p className='m-0'>Пожалуй я выберу Js</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:29</span>
+                            </div>
+                        </div>
+                        <div className="message message__reactTutor d-flex flex-column w-100 align-items-start">
+                            <div className='header_message header_message__reactTutor'>
+                                <img src={largeLogo} width='35px' alt="largeLogo"/>
+                                <span className='ps-2'>ReactTutor</span>
+                            </div>
+                            <div className='content_message content_message__reactTutor mt-3'>
+                                <p className='m-0'>Привет! Я ReactTutor, твой помощник в изучении фронтенда.</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:27</span>
+                            </div>
+                        </div>
+                        <div className="message message__user d-flex flex-column w-100 align-items-end">
+                            <div className='header_message header_message__user'>
+                                <span className='pe-2'>Александр</span>
+                                <img src={userAvatar} width='35px' alt="largeLogo"/>
+                            </div>
+                            <div className='content_message content_message__user mt-3'>
+                                <p className='m-0'>Пожалуй я выберу Js</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:29</span>
+                            </div>
+                        </div>
+                        <div className="message message__reactTutor d-flex flex-column w-100 align-items-start">
+                            <div className='header_message header_message__reactTutor'>
+                                <img src={largeLogo} width='35px' alt="largeLogo"/>
+                                <span className='ps-2'>ReactTutor</span>
+                            </div>
+                            <div className='content_message content_message__reactTutor mt-3'>
+                                <p className='m-0'>Привет! Я ReactTutor, твой помощник в изучении фронтенда.</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:27</span>
+                            </div>
+                        </div>
+                        <div className="message message__user d-flex flex-column w-100 align-items-end">
+                            <div className='header_message header_message__user'>
+                                <span className='pe-2'>Александр</span>
+                                <img src={userAvatar} width='35px' alt="largeLogo"/>
+                            </div>
+                            <div className='content_message content_message__user mt-3'>
+                                <p className='m-0'>Пожалуй я выберу Js</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:29</span>
+                            </div>
+                        </div>
+                        <div className="message message__reactTutor d-flex flex-column w-100 align-items-start">
+                            <div className='header_message header_message__reactTutor'>
+                                <img src={largeLogo} width='35px' alt="largeLogo"/>
+                                <span className='ps-2'>ReactTutor</span>
+                            </div>
+                            <div className='content_message content_message__reactTutor mt-3'>
+                                <p className='m-0'>Привет! Я ReactTutor, твой помощник в изучении фронтенда.</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:27</span>
+                            </div>
+                        </div>
+                        <div className="message message__user d-flex flex-column w-100 align-items-end">
+                            <div className='header_message header_message__user'>
+                                <span className='pe-2'>Александр</span>
+                                <img src={userAvatar} width='35px' alt="largeLogo"/>
+                            </div>
+                            <div className='content_message content_message__user mt-3'>
+                                <p className='m-0'>Пожалуй я выберу Js</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:29</span>
+                            </div>
+                        </div>
+                        <div className="message message__reactTutor d-flex flex-column w-100 align-items-start">
+                            <div className='header_message header_message__reactTutor'>
+                                <img src={largeLogo} width='35px' alt="largeLogo"/>
+                                <span className='ps-2'>ReactTutor</span>
+                            </div>
+                            <div className='content_message content_message__reactTutor mt-3'>
+                                <p className='m-0'>Привет! Я ReactTutor, твой помощник в изучении фронтенда.</p>
+                            </div>
+                            <div className='footer_message mt-1'>
+                                <span>20:27</span>
+                            </div>
+                        </div>
                     </div>
-                    <Form className='mt-auto p-2 container-fluid pb-md-5 chatFormWrapper position-fixed bottom-0' ref={chatFormWrapperRef}>
+                    <Form className='mt-auto pe-3 container-fluid pb-md-5 pb-3 chatFormWrapper position-fixed bottom-0'
+                          ref={chatFormWrapperRef}>
                         <Form.Group className="row chat_input" ref={chatInputElementRef} controlId="formChat">
                             <div className="col-10 col-md-11 ps-0">
-                                <Form.Control as='textarea' className='form-control__chat' ref={formControlChatRef} rows={1}
+                                <Form.Control as='textarea' className='form-control__chat' ref={formControlChatRef}
+                                              rows={1}
                                               onInput={handleInput} onChange={handleChangeTextarea}
                                               placeholder="Ваш вопрос"/>
                             </div>
                             <div
-                                className="col-2 ps-md-4 ps-lg-2 ps-xxl-3 ps-0 m-0 col-md-1 d-flex" ref={wrapperButtonSubmitRef}>
+                                className="col-2 ps-md-4 ps-lg-2 ps-xxl-3 ps-0 m-0 col-md-1 d-flex"
+                                ref={wrapperButtonSubmitRef}>
                                 <Button className='button_submit disabled' ref={submitButton}>
                                     <img src={send} alt='Отправить'/>
                                 </Button>
